0e27929eec86e370991b52eb9d5047b2f0361a55
agschmid <agschmid@andrew.cmu.edu>
2023-10-26 23:11:29 -0400

    added game functionality

--- js/main.js
+++ js/main.js
@@ -5,19 +5,58 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xbfe3dd );
 
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 5000 );
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.shadowMap.enabled = true;
+
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.SphereGeometry( 1, 32, 16 );
-const material = new THREE.MeshNormalMaterial();
+
+let sphereRadius = 0.5
+
+const geometry = new THREE.SphereGeometry(sphereRadius, 32, 16 );
+const material = new THREE.MeshStandardMaterial({color:0xffffff});
 const sphere = new THREE.Mesh( geometry, material );
+
+sphere.castShadow = true;
+sphere.receiveShadow = true;
+
+
 scene.add( sphere );
 
-const light = new THREE.AmbientLight( 0x404040 ); // soft white light
-scene.add( light );
+
+let paddleWidth = 2
+let paddleHeight = 1.5
+
+const paddleGeom = new THREE.PlaneGeometry( paddleWidth, paddleHeight );
+const paddleMaterial = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide, transparent: true, opacity: 0.3} );
+const paddle = new THREE.Mesh( paddleGeom, paddleMaterial );
+scene.add( paddle );
+
+
+let light = new THREE.DirectionalLight(0xFFFFFF, 2.0);
+light.position.set(-2,-0.5,-10);
+light.target.position.set(1, 2, -10);
+light.castShadow = true;
+scene.add(light);
+
+let light2 = new THREE.DirectionalLight(0xFFFFFF, 3.0);
+light2.position.set(0.1,0.2,0.9);
+light2.target.position.set(-0.5, -0.5, -10);
+light2.castShadow = true;
+scene.add(light2);
+
+
+const boxgeometry = new THREE.BoxGeometry( 5, 5, 12 ); 
+const boxmaterial = new THREE.MeshStandardMaterial( {color: 0x222222, side: THREE.BackSide} ); 
+const box = new THREE.Mesh( boxgeometry, boxmaterial ); 
+box.position.z=-4
+box.castShadow = false;
+box.receiveShadow = true;
+
+scene.add( box );
 
 camera.position.z = 5;
 
@@ -30,7 +69,12 @@ let updateRate = 1/60;
 
 let divLog = document.querySelector('#log')
 
-
+let sx = 0
+let sy = 0
+let sz = -0.5
+let svx = 0.0; // Velocity x and y
+let svy = 0.0;
+let svz = 1.0;
 
 animate();
 
@@ -52,7 +96,7 @@ let zMult = 0
 
 shootButton.addEventListener('click', ()=>{
     if (!playing){
-        zMult = 1
+        zMult = -1
     }
     playing=true
 })
@@ -63,7 +107,9 @@ function animate() {
 
 	renderer.render( scene, camera );
 
-    sphere.position.set(px, py, pz)
+    paddle.position.set(px, py, pz)
+    sphere.position.set(sx, sy, sz)
+
 }
 
 function getAccel(){
@@ -101,20 +147,70 @@ function getAccel(){
                     vy = 0;
                 }
 
-                pz = pz+0.1*zMult
-
-
-                if (pz < -10){
-                    zMult=1
-                }
-                if (pz > 0) {
-                    zMult=-1
+                sx = sx - svx*0.1;
+                if (sx > 2 || sx < -2){ 
+                    sx = Math.max(-2, Math.min(2, sx)) // Clip px between 0-98
+                    svx = -svx;
                 }
 
-                // divLog.innerText=pz
+                sy = sy - svy*0.1;
+                if (sy > 2 || sy < -2){
+                    sy = Math.max(-2, Math.min(2, sy)) // Clip py between 0-98
+                    svy = -svy;
+                }
 
 
+                sz = sz+0.1*svz*zMult
+
+                if (playing){
+                    if (sz < -10){
+                        zMult=1
+                    }
+    
+                    if (sz > -0.5) {
+    
+                        let a=Math.abs(px-sx)
+                        let b=Math.abs(py-sy)
+                        if (didPaddleHit(a-0.5*paddleWidth,b-0.5*paddleHeight)){
+
+                            zMult=-1
+                            let alpha = a*2/paddleWidth
+                            let beta = b*2/paddleHeight
+
+                            let vScale = (1 / (alpha**2 + beta**2 + 1))**0.5
+
+                            svx = Math.sign(px-sx)*vScale*alpha
+                            svy = Math.sign(py-sy)*vScale*beta
+                            svz = vScale
+
+                        } else {
+                            zMult=0
+                            svx = 0
+                            svy = 0
+                            svz=1
+                            playing = false
+                        }
+                            
+                    }
+    
+
+                }
             });
         }
     });
+}
+
+
+
+// https://stackoverflow.com/questions/47639413/algorithm-for-accurate-detection-of-overlap-between-a-square-and-a-circle
+function didPaddleHit(a, b){
+    if (a>0) {
+        if (b>0) {
+            return a**2 + b**2<sphereRadius**2;
+        } else {
+            return a<sphereRadius;
+        }
+    } else {
+        return b<sphereRadius;
+    }
 }
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import mkcert from 'vite-plugin-mkcert'
 export default defineConfig({
   server: { https: true },
   plugins: [ mkcert() ]
+  
 })
